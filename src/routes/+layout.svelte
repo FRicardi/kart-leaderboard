8fcd0dab04e46508b688e9846ee8f2968deac442
Felipe Ricardi <felipe.ricardi@sap.com>
2023-07-26 13:02:57 -0300

    chore: creating base project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+ import { onMount } from "svelte";
+
+ onMount(async () => {
+     await import('@ui5/webcomponents-fiori/dist/ShellBar');
+     await import('@ui5/webcomponents/dist/list.js')
+ })
+
+</script>
+
+<ui5-shellbar primary-title="Gauchão de Kart">
+    <ui5-li slot="menuItems">
+        Corridas
+    </ui5-li>
+    <ui5-li slot="menuItems">
+        Leaderboard
+    </ui5-li>
+</ui5-shellbar>
