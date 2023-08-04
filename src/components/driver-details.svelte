91aaff9f2c160fc9a049c5008859fd194de41e53
Felipe Ricardi <felipe.ricardi@sap.com>
2023-08-04 17:15:52 -0300

    feat: creating component for driver details

---
+++ src/components/driver-details.svelte
@@ -0,0 +1,23 @@
+<script>
+ import { onMount } from 'svelte';
+
+ onMount(async () => {
+	 await import('@ui5/webcomponents/dist/Title');
+	 await import('@ui5/webcomponents-fiori/dist/Page.js');
+ });
+
+ export let driverId;
+</script>
+
+<style>
+ .driver-header {
+     padding: 20px 10px
+ }
+</style>
+
+<ui5-page>
+	<div class="driver-header" slot="header">
+		<ui5-title level="H3">{driverId}</ui5-title>
+		<div>Races: 10</div>
+	</div>
+</ui5-page>
